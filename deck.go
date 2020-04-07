a630825b35666d98faac21b0f382d97f615ba06a
Anorth1997 <chenziyao1997@hotmail.com>
2020-04-07 18:21:18 -0400

    use pointer and receiver

--- deck.go
+++ deck.go
@@ -7,8 +7,28 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func (d *deck) deal(handSize int) deck {
+	hand := (*d)[:handSize]
+	*d = (*d)[handSize:]
+	return hand
 }
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 func main() {
 	// var card string = "Ace of Spades"
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
+	hand := (&cards).deal(5)
+
+	hand.print()
 	cards.print()
 }
 
